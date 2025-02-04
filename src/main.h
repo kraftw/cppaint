2512415b2a2a986de6109221af05d7b947830802
kraftw <kraftworker11@gmail.com>
2025-02-04 17:08:52 -0500

    fixed green <-> blue

--- src/main.h
+++ src/main.h
@@ -15,8 +15,8 @@ std::vector<sf::Color> colors =
     sf::Color(255, 0,   0),    // 0: RED
     sf::Color(255, 160, 0),    // 1: ORANGE
     sf::Color(255, 255, 0),    // 2: YELLOW
-    sf::Color(0,   0,   255),  // 3: GREEN
-    sf::Color(0,   255, 0),    // 4: BLUE
+    sf::Color(0,   255, 0),    // 3: GREEN
+    sf::Color(0,   0,   255),  // 4: BLUE
     sf::Color(60,  0,   255),  // 5: INDIGO
     sf::Color(255, 0,   255),  // 6: VIOLET
 };
